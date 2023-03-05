21de2833f97ba33c549fe77ce3269938c5ecbdbe
Gérald Gaillard <gerald8142@gmail.com>
2023-03-05 14:39:33 +0100

    create middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  console.log("traitement de lauthentification");
+  try {
+    if (!req.headers.authorization)
+      throw new Error("Header Authorization not specified ! ");
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error });
+  }
+};
